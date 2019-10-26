8e7b7b91fb055eb3a2eff8e035b6ac699fab9469
Lee <winterfive@users.noreply.github.com>
2019-10-26 10:21:02 -0400

    Added modal code

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-$(document).ready(function () {
-    console.log("Document ready");
-});
+$(document).ready(function() {
+  console.log("Document ready")
+
+  $("#submit-form").on("submit", function(e) {
+    $("#myModal").modal("show")
+    e.preventDefault()
+  })
+
+  $("#myModal").on("shown.bs.modal", function() {
+    $("#myInput").trigger("focus")
+  })
+})
